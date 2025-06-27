da6012c6574768970d623ae9f97a98ae0d3538cd
Dmitrii <kalla.mallanik@gmail.com>
2025-06-27 19:48:14 +0300

    create server for receiving metrics

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	db "github.com/dmitastr/yp_observability_service/internal/database"
+	"github.com/dmitastr/yp_observability_service/internal/service"
+	"github.com/dmitastr/yp_observability_service/internal/handlers/update_metric"
+)
+
+
+func main() {
+	storage := db.NewStorage()
+	service := service.NewService(storage)
+	metricHandler := updatemetric.NewHandler(service)
+
+	mux := http.NewServeMux()
+	mux.Handle(`POST /update/{mtype}/{name}/{value}`, *metricHandler)
+	
+	if err := http.ListenAndServe(`:8080`, mux); err != nil {
+		panic(err)
+	}
+}
