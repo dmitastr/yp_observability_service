b3debeeeb90b229ce9841b00291c69a47f8a28db
Dmitrii <kalla.mallanik@gmail.com>
2025-07-01 14:21:04 +0300

    add command line args for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 
 	db "github.com/dmitastr/yp_observability_service/internal/database"
 	"github.com/dmitastr/yp_observability_service/internal/domain/service"
-	"github.com/dmitastr/yp_observability_service/internal/presentation/handlers/update_metric"
 	"github.com/dmitastr/yp_observability_service/internal/presentation/handlers/get_metric"
 	"github.com/dmitastr/yp_observability_service/internal/presentation/handlers/list_metric"
-
+	"github.com/dmitastr/yp_observability_service/internal/presentation/handlers/update_metric"
 )
+
+var serverAddress string
+func init() {
+	flag.StringVar(&serverAddress, "a", "localhost:8080", "set server host and port")
+}
 
 
 func main() {
+	flag.Parse()
+
 	storage := db.NewStorage()
 	service := service.NewService(storage)
 	metricHandler := updatemetric.NewHandler(service)
@@ -27,7 +34,7 @@ func main() {
 	router.Post(`/update/{mtype}/{name}/{value}`, metricHandler.ServeHTTP)
 	router.Get(`/value/{mtype}/{name}`, getMetricHandler.ServeHTTP)
 
-	if err := http.ListenAndServe(`:8080`, router); err != nil {
+	if err := http.ListenAndServe(serverAddress, router); err != nil {
 		panic(err)
 	}
 }
