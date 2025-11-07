a8baeb2113bfa658b4444005441323a0f8c9f2d4
Dmitrii <kalla.mallanik@gmail.com>
2025-11-07 21:23:53 +0300

    fix names in config file, remove unused json tags

--- internal/config/env_parser/server/server_env_config/env_config.go
+++ internal/config/env_parser/server/server_env_config/env_config.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Config struct {
-	Address         *string `env:"ADDRESS" mapstructure:"address" json:"address"`
-	StoreInterval   *int    `env:"STORE_INTERVAL" mapstructure:"store_interval" json:"store_interval"`
+	Address         *string `env:"ADDRESS" mapstructure:"address"`
+	StoreInterval   *int    `env:"STORE_INTERVAL" mapstructure:"store_interval"`
 	FileStoragePath *string `env:"FILE_STORAGE_PATH" mapstructure:"store_file"`
-	Restore         *bool   `env:"RESTORE" mapstructure:"restore" json:"restore"`
-	DBUrl           *string `env:"DATABASE_DSN" mapstructure:"database_dsn" json:"database_dsn"`
+	Restore         *bool   `env:"RESTORE" mapstructure:"restore"`
+	DBUrl           *string `env:"DATABASE_DSN" mapstructure:"database_dsn"`
 	Key             *string `env:"KEY" mapstructure:"k"`
 	AuditFile       *string `env:"AUDIT_FILE" mapstructure:"audit-file"`
 	AuditURL        *string `env:"AUDIT_URL" mapstructure:"audit-url"`
-	PrivateKeyPath  *string `env:"CRYPTO_KEY" mapstructure:"crypto-key" json:"crypto_key"`
+	PrivateKeyPath  *string `env:"CRYPTO_KEY" mapstructure:"crypto-key"`
 }
 
 func (c *Config) String() string {
